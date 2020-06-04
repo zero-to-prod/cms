4a43b76e348d684337485d0b6f69d0e9ae9581ad
David Smith <davidsmith@itsandbox.net>
2020-06-04 07:43:50 -0400

    Initial commit.

---
+++ app/Console/Commands/ModelMake.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Foundation\Console\ModelMakeCommand;
+
+class ModelMake extends ModelMakeCommand
+{
+
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace.'\\Models';
+    }
+}
