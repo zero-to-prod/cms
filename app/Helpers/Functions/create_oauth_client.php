91f798e995767f3b4660857c579db1ba686e5fe4
David Smith <davidsmith@itsandbox.net>
2020-06-05 11:44:13 -0400

    Create create_oauth_client().

---
+++ app/Helpers/Functions/create_oauth_client.php
@@ -0,0 +1,33 @@
+<?php
+
+use Laravel\Passport\ClientRepository;
+
+if ( ! function_exists('create_oauth_client')) {
+    /**
+     * @param $name
+     * @param  null  $userId
+     * @param $redirect
+     * @param  null  $provider
+     * @param  bool  $personalAccess
+     * @param  bool  $password
+     * @param  bool  $confidential
+     *
+     * @return mixed
+     *
+     * @see \Tests\Unit\Helpers\Functions\CreateOauthClientTest
+     */
+    function create_oauth_client(
+        $name,
+        $userId = '',
+        $redirect = '',
+        $provider = '',
+        $personalAccess = false,
+        $password = false,
+        $confidential = true
+    ) {
+        $ClientRepository = new ClientRepository();
+
+        return $ClientRepository->create($userId, $name, $redirect, $provider, $personalAccess, $password,
+            $confidential);
+    }
+}
---
+++ tests/Unit/Helpers/Functions/CreateOauthClientTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Unit\Helpers\Functions;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+/** @see create_oauth_client() */
+class CreateOauthClientTest extends TestCase
+{
+
+    use DatabaseMigrations;
+    use DatabaseTransactions;
+
+    /** @test */
+    public function see_if_function_exits(): void
+    {
+        $this->assertTrue(function_exists('create_oauth_client'));
+    }
+
+    /** @test */
+    public function create_oauth_client(): void
+    {
+        $name           = 'Test Client';
+        $user_id        = 100;
+        $redirect       = 'url.domain';
+        $provider       = 'provider';
+        $personalAccess = true;
+        $password       = true;
+        $confidential   = true;
+
+        $oauth_client = create_oauth_client($name, $user_id, $redirect, $provider, $personalAccess, $password,
+            $confidential);
+        $this->assertEquals($name, $oauth_client->name);
+        $this->assertEquals($user_id, $oauth_client->user_id);
+        $this->assertEquals($redirect, $oauth_client->redirect);
+        $this->assertTrue($oauth_client->personal_access_client);
+        $this->assertTrue($oauth_client->password_client);
+        $this->assertIsString($oauth_client->secret);
+    }
+}
