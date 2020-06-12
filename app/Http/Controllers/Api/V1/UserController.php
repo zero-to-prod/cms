e919962d05f3caa4a45a839734f56c625a396b2b
David Smith <davidsmith@itsandbox.net>
2020-06-12 13:01:17 -0400

    Implement Oauth login, logout working.

---
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Response;
+
+class UserController extends Controller
+{
+    public function __invoke(Response $response)
+    {
+        return response(auth()->user(), 200);
+    }
+}
