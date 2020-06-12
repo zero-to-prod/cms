1fdb3454bd3f942797d5ad45d82518f223f4acab
David Smith <davidsmith@itsandbox.net>
2020-06-12 16:52:58 -0400

    Update routes.

--- routes/api.php
+++ routes/api.php
@@ -4,21 +4,20 @@ use CloudCreativity\LaravelJsonApi\Facades\JsonApi;
 use Illuminate\Support\Facades\Route;
 
 JsonApi::register('default')->routes(static function ($api) {
+    Route::namespace('Api')->group(static function () {
+        Route::post('/register', 'RegisterController')->withoutMiddleware('auth:api');
+        Route::post('/login', 'LoginController')->withoutMiddleware('auth:api');
+        Route::post('/logout', 'LogoutController');
+
+        Route::group(['namespace' => 'V1', 'middleware' => 'client'], static function () {
+            Route::get('/user', 'UserController');
+        });
+    });
+
     $api->resource('product_types')->relationships(static function ($relations) {
         $relations->hasMany('products');
     });
     $api->resource('products')->relationships(static function ($relations) {
         $relations->hasOne('product_types');
-    });
-
-    Route::namespace('Api')->group(static function () {
-        Route::post('/login', 'LoginController')->withoutMiddleware('auth:api');
-        Route::post('/logout', 'LogoutController')->middleware('auth:api');
-
-        Route::namespace('V1')->group(static function () {
-            Route::group(['middleware' => 'client'], static function () {
-                Route::get('/user', 'UserController');
-            });
-        });
     });
 });
