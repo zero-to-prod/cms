924e4d1e4c23d3b426ba6cffbaf21d1ea2015546
David Smith <davidsmith@itsandbox.net>
2020-06-15 08:21:38 -0400

    Update defaultStringLength()

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Schema::defaultStringLength(249);
+        Schema::defaultStringLength(191);
     }
 }
