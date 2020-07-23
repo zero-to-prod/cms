2527b13da756d300aff589ae9a74585a6c3625a3
zero-to-prod <61474950+zero-to-prod@users.noreply.github.com>
2020-07-23 08:21:27 -0400

    Apply fixes from StyleCI (#25)

--- app/Events/ApiLogoutEvent.php
+++ app/Events/ApiLogoutEvent.php
@@ -10,7 +10,6 @@ use Illuminate\Queue\SerializesModels;
 
 class ApiLogoutEvent
 {
-
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
     /**
--- app/Http/Middleware/RequestLog.php
+++ app/Http/Middleware/RequestLog.php
@@ -9,7 +9,6 @@ use Illuminate\Http\Request;
 
 class RequestLog
 {
-
     /**
      * Handle an incoming request.
      *
