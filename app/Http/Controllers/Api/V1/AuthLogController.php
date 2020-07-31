8a67496b0e7e3b4109ca7b82d90a46703d7e79c7
David Smith <davidsmith@itsandbox.net>
2020-07-31 16:58:50 -0400

    WIP

--- app/Http/Controllers/Api/V1/AuthLogController.php
+++ app/Http/Controllers/Api/V1/AuthLogController.php
@@ -4,12 +4,22 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Models\AuthLog;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AuthLogController extends Controller
 {
+
+    /**
+     * @param  Response  $response
+     *
+     * @return Application|ResponseFactory|Response
+     */
     public function __invoke(Response $response)
     {
-        return response(AuthLog::with('user')->orderBy('created_at', 'DESC')->limit(10)->get(), 200);
+        /** @todo Make test. */
+        return response(AuthLog::where('user_id', Auth::user()->id)->orderBy('created_at', 'DESC')->limit(10)->get(['login', 'created_at', 'ip_address','user_agent']), 200);
     }
 }
